32e91f9027e9e27abb27ff4679bb5b2e80c32812
Christian Daniel Ortiz Ororbia <45889252+cortiz2894@users.noreply.github.com>
2024-11-04 21:57:30 -0300

    Feature: Adding textures (#5)
    * feat: Adding audio and file selector
    
    * feat: Adding textures and improving player

--- components/Player.jsx
+++ components/Player.jsx
@@ -36,8 +36,17 @@ const PLAYLIST = [
 ];
 
 const AudioPlayer = ({ zoomToScreen, audioControls }) => {
-  const { isPlaying, playAudio, pauseAudio, fileName, loadAudio } =
-    audioControls;
+
+  const {
+    isPlaying,
+    playAudio,
+    pauseAudio,
+    fileName,
+    loadAudio,
+    duration,
+    currentTime,
+  } = audioControls;
+
   const columnRefs = useRef([]);
   const intervalRef = useRef(null);
   const fileNameRef = useRef(null);
@@ -114,7 +123,7 @@ const AudioPlayer = ({ zoomToScreen, audioControls }) => {
 
   return (
     <div
-      className="flex flex-col bg-[#242424] h-full w-full absolute left-0 top-0 cursor-pointer px-4 py-3 gap-3"
+      className="flex flex-col bg-[#242424] h-full w-full absolute left-0 top-0 cursor-pointer px-8 py-6 gap-3"
       onClick={zoomToScreen}
     >
       <div className="w-full flex justify-between">
@@ -132,7 +141,7 @@ const AudioPlayer = ({ zoomToScreen, audioControls }) => {
         <LiaBatteryHalfSolid className="text-[#4e85fb] text-2xl" />
       </div>
       <div className="flex gap-5 h-full">
-        <div className="w-2/3 h-[90%] flex flex-col justify-end">
+        <div className="w-2/3 h-[80%] flex flex-col justify-end">
           <div className="pixel-wave-container w-full">
             {Array.from({ length: COLUMNS }).map((_, columnIndex) => (
               <div
@@ -152,29 +161,30 @@ const AudioPlayer = ({ zoomToScreen, audioControls }) => {
               </div>
             ))}
           </div>
-          <div className="flex gap-2 mt-4 w-full">
+          <div className="flex mt-4 gap-3 w-[80%]">
             <button
               onClick={() => {
-                playAudio();
+                isPlaying ? pauseAudio() : playAudio();
               }}
             >
-              <IoMdPlay className="text-[#4e85fb] text-xl" />
+              {isPlaying ? (
+                <IoMdPause className="text-[#4e85fb] text-xl" />
+              ) : (
+                <IoMdPlay className="text-[#4e85fb] text-xl" />
+              )}
             </button>
-            <button
-              onClick={() => {
-                pauseAudio();
-              }}
-            >
-              <IoMdPause className="text-[#4e85fb] text-xl" />
-            </button>
+            <div className="w-full flex justify-between">
+              <span className="text-[#4e85fb] text-lg">{currentTime}</span>
+              <span className="text-[#4e85fb] text-lg">{duration}</span>
+            </div>
           </div>
         </div>
         <div
-          className="border border-[#4e85fb] w-1/3 h-[90%] overflow-y-scroll overflow-x-hidden [&::-webkit-scrollbar]:w-2
+          className="border border-[#4e85fb] w-1/3 h-[80%] overflow-y-scroll overflow-x-hidden [&::-webkit-scrollbar]:w-2
         [&::-webkit-scrollbar-track]:bg-[#242424]
         [&::-webkit-scrollbar-thumb]:bg-[#4e85fb]"
         >
-          <p className="font-minicraftia bg-[#4e85fb] p-2 text-black text-sm">
+          <p className="font-minicraftia bg-[#4e85fb] p-2 text-[#242424] text-sm">
             Playlist
           </p>
           <ul className="flex flex-col border-r border-[#4e85fb]">
@@ -185,7 +195,9 @@ const AudioPlayer = ({ zoomToScreen, audioControls }) => {
                   className={`
                   ${
                     file.fileName === fileName
-                      ? "text-black bg-[#4b68a5]"
+
+                      ? "text-[#242424] bg-[#4e85fbbd] border border-[#242424]"
+
                       : "text-[#4e85fb] hover:bg-[#3d3d3d]"
                   } 
                   px-2 py-2  text-sm font-minicraftia text-ellipsis overflow-hidden whitespace-nowrap`}
---
+++ components/textures/MeshHoleTexture.jsx
@@ -0,0 +1,48 @@
+import { useTexture } from "@react-three/drei";
+import * as THREE from "three";
+import { useControls } from "leva";
+
+const MeshHoleTexture = ({ geometry }) => {
+  const textures = useTexture({
+    map: "/textures/meshHoles/SheetMetal002_2K-PNG_Color.png",
+    displacementMap:
+      "/textures/meshHoles/SheetMetal002_2K-PNG_Displacement.png",
+    metalnessMap: "/textures/meshHoles/SheetMetal002_2K-PNG_Metalness.png",
+    normalMap: "/textures/meshHoles/SheetMetal002_2K-PNG_NormalDX.png",
+    alphaMap: "/textures/meshHoles/SheetMetal002_2K-PNG_Opacity.png",
+    // roughnessMap: "/textures/meshHoles/SheetMetal002_2K-PNG_Roughness.png",
+  });
+
+  const { textureScale, ...textureProps } = useControls("Mesh Hole", {
+    displacementScale: { value: 0.05, min: 0, max: 1, step: 0.01 },
+    metalness: { value: 0.51, min: 0, max: 1, step: 0.01 },
+    roughness: { value: 0.3, min: 0, max: 1, step: 0.01 },
+    textureScale: { value: 0.85, min: 0, max: 10, step: 0.01 },
+  });
+
+  Object.values(textures).forEach((texture) => {
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(textureScale, textureScale);
+  });
+
+  return (
+    <mesh
+      castShadow
+      receiveShadow
+      geometry={geometry}
+      position={[-2.027, -0.406, 0.071]}
+      rotation={[Math.PI / 2, 0, 0]}
+      scale={0.111}
+    >
+      <meshStandardMaterial
+        {...textures}
+        {...textureProps}
+        transparent={true}
+        color={"#37363d"}
+      />
+    </mesh>
+  );
+};
+
+export { MeshHoleTexture };
